2e7fd083f6566977ae1c2b5121ebb8301568b4ad
koziejka <koziejka.com@gmail.com>
2016-05-28 12:18:52 +0200

    Update fb.js

--- js/fb.js
+++ js/fb.js
@@ -10,8 +10,7 @@ $(document).ready(function(){
         if(r.status === 'connected'){
             FB.api('/me', 'GET', {fields: 'name,picture'}, function(response) {
                 console.log(response);
-                $("#FBinfo").parent().children("a").children("div").html("<img src='" + response.picture.data.url + "'>" + response.name);
-                $(".hidden").removeAttr("hidden")
+                $("#FBinfo").parent().children("a").children("div").html("<img src='" + response.picture.data.url + "'>" + response.name).removeClass("hidden");
             });
         }
     });
